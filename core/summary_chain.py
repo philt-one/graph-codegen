cb6c20d729e6f4d85d35f18c2927d4a0db7807fe
p.talarico <p.talarico@finway.de>
2023-10-29 05:29:39 +0100

    Change prompt and upgrade chunk_size

--- core/summary_chain.py
+++ core/summary_chain.py
@@ -6,14 +6,14 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter, Language
 
 def summarise_code(code):
     python_splitter = RecursiveCharacterTextSplitter.from_language(
-        language=Language.PYTHON, chunk_size=200, chunk_overlap=0
+        language=Language.PYTHON, chunk_size=1000, chunk_overlap=0
     )
     python_docs = python_splitter.create_documents([code])
 
     summary_chain = (
         {"code": lambda x: x.page_content}
         | ChatPromptTemplate.from_template(
-            "Summarize the following python code:\n\n{doc}"
+            "Provide a short summary for the following python code in one line:\n\n{code}"
         )
         | ChatOpenAI(max_retries=0)
         | StrOutputParser()
