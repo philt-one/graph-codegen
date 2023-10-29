9284986a112d19437ddc7bb7a03ca7db85fb11aa
p.talarico <p.talarico@finway.de>
2023-10-29 02:16:41 +0200

    add code_extractor

---
+++ core/code_extractor.py
@@ -0,0 +1,16 @@
+import ast
+import os
+
+
+class PyAstExtractor:
+    def __init__(self, path: str | os.PathLike):
+        self.path = path
+        self.name = self._remove_extension(os.path.basename(path))
+        self.tree = self.get_tree()
+
+    def _remove_extension(self, filename) -> str:
+        return os.path.splitext(filename)[0]
+
+    def get_tree(self) -> ast.Module:
+        with open(self.path, "r") as source:
+            return ast.parse(source.read())
