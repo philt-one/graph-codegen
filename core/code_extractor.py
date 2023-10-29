6763f9ff10d6de63baf8b93e37e02a56dd716a18
p.talarico <p.talarico@finway.de>
2023-10-29 03:56:52 +0100

    Add methods to get objects from PyAstExtractor

--- core/code_extractor.py
+++ core/code_extractor.py
@@ -1,17 +1,37 @@
 import ast
 import os
+from typing import List, Dict
 
 
 class PyAstExtractor:
     def __init__(self, path: str | os.PathLike):
         self.path = path
         self.name = self._remove_extension(os.path.basename(path))
-        self.tree = self.get_tree()
-        self.body = self.tree.body
+        self.tree = self._get_tree()
 
     def _remove_extension(self, filename) -> str:
         return os.path.splitext(filename)[0]
 
-    def get_tree(self) -> ast.Module:
+    def _get_tree(self) -> ast.Module:
         with open(self.path, "r") as source:
             return ast.parse(source.read())
+
+    def get_functions(self) -> List[str]:
+        """Extract all top-level function nodes from the AST."""
+        return [node for node in self.tree.body if isinstance(node, ast.FunctionDef)]
+
+    def get_classes(self) -> List[str]:
+        """Extract all top-level class nodes from the AST."""
+        return [node for node in self.tree.body if isinstance(node, ast.ClassDef)]
+
+    def get_imports(self) -> List[str]:
+        """Extract all top-level imported modules nodes from the AST."""
+        return [node for node in self.tree.body if isinstance(node, ast.Import)]
+
+    def get_dependencies(self) -> Dict[str, List[str]]:
+        """Extract all dependencies (imports, functions, classes) from the AST."""
+        return {
+            "imports": self.get_imports(),
+            "functions": self.get_functions(),
+            "classes": self.get_classes(),
+        }
